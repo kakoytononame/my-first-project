1ea4ef6d1cf762e9b5d1d90df2df81cc21e0c3eb
тимур валиев <95782287t.valiev@bars-open.ru>
2024-03-22 13:19:19 +0300

    Update validateTaskData.js

--- middleware/validateTaskData.js
+++ middleware/validateTaskData.js
@@ -1,7 +1,7 @@
 export const validateTaskData = (req, res, next) => {
     const { description, status} = req.body;
 
-    if(description || typeof description !== "string"){
+    if(!description || typeof description !== "string"){
         return res.status(400).json({ message: 'Invalid or missind description'})
     }
 
